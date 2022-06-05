de7ba6c80aa54b49aff344a2cbca231feaf891cd
Antônio Luis <antonioluisp97@gmail.com>
2022-06-05 18:35:15 -0300

    desafio 1 e 2 chapter 2

---
+++ trilha-node/chapter 2/desafios/chapter-2-desafio-1-Introducao-ao-solid/src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,21 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists) throw new Error("User not found");
+    if (userExists.admin === false) throw new Error("User not admin");
+
+    return this.usersRepository.list();
+  }
+}
+
+export { ListAllUsersUseCase };
